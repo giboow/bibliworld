5a374346231af682f4781ff39e08b8b1de7ebda7
giboow <philippe.gibert@gmail.com>
2014-02-09 18:39:02 +0100

    disable swagger test route

--- API/configs/routes.js
+++ API/configs/routes.js
@@ -4,5 +4,5 @@ module.exports = function (app) {
     require('../routes/index')(app);
     require('../routes/user')(app);
     require('../routes/book')(app);
-    require('../routes/testSwagger')(app);
+    //require('../routes/testSwagger')(app);
 };

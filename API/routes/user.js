f6eb48c42b699093f1ce046039e8afc0e2108c6e
giboow <philippe.gibert@gmail.com>
2014-02-10 18:38:20 +0100

    correction on user routes

--- API/routes/user.js
+++ API/routes/user.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = function (app) {
+module.exports = function () {
 
 	var swagger = require('swagger-node-express');
 
@@ -23,7 +23,7 @@ module.exports = function (app) {
 			responseMessages : [
 				swagger.errors.invalid('username'),
 				swagger.errors.invalid('password'),
-				swagger.errors.invalid('username/password')
+				swagger.errors.invalid('user/password')
 			]
 		},
 		'action': function (req, res) {
@@ -56,11 +56,14 @@ module.exports = function (app) {
 						};
 
 						return res.send(datas);
+					} else {
+						return res.send({
+							'code' : 400,
+							'message' : 'Invalid user/password'
+						}, 400);
 					}
 				});
 			}
-
-			throw swagger.errors.invalid('username/password');
 		}
 	};
 	swagger.addPost(userLogin);
@@ -76,7 +79,7 @@ module.exports = function (app) {
 			nickname : 'userLogout',
 			produces : ['application/json'],
 		},
-		'action': function (req,res) {
+		'action': function (request, response) {
 			request.session.destroy(function () {
 			
 				return response.send({
@@ -89,32 +92,59 @@ module.exports = function (app) {
 	swagger.addGet(userLogout);
 
 
-	
+	var userRegister = {
+		'spec': {
+			description : 'Log into application',
+			path : '/user/register',
+			method: 'POST',
+			summary : 'Register user',
+			notes : 'Returns an array info with id and username infos',
+			type : 'array',
+			nickname : 'userRegister',
+			produces : ['application/json'],
+			parameters : [
+				swagger.queryParam('username', 'User name', 'string', true),
+				swagger.queryParam('password', 'User password', 'string', true),
+				swagger.queryParam('email', 'User email', 'string', true),
+			],
+			responseMessages : [
+				swagger.errors.invalid('username'),
+				swagger.errors.invalid('password'),
+				swagger.errors.invalid('email'),
+				swagger.errors.invalid('User allready exist'),
+			]
+		},
+		'action': function (req, res) {
+			var username  = req.query.username,
+				email = req.query.email,
+				password = req.query.password;
 
-
-	app.get('/user/register', function (req, res) {
-		var datas = {error : false};
-
-		var username  = req.query.username,
-			email = req.query.email,
-			password = req.query.password;
-
-		User.register(username, email, password, function (err, user) {
-			if (err) {
-				datas.error  = {
-					code : 102,
-					message : 'User allready Exist'
-				};
-			} else {
-				datas.user = {
-					username : user.username,
-					email : user.email
-				};
+			if (!username) {
+				throw swagger.errors.invalid('username');
+			}
+			if (!password) {
+				throw swagger.errors.invalid('password');
+			}
+			if (!email) {
+				throw swagger.errors.invalid('email');
 			}
 
-			res.send(datas);
-		});
-	});
+			User.register(username, email, password, function (err, user) {
+				if (!err) {
+					return res.send({
+						username : user.username,
+						email : user.email
+					});
+				} else {
+					return res.send({
+						code : 400,
+						message : "invalid User allready exist"
+					}, 400);
+				}
+			});
+		}
+	};
+	swagger.addGet(userRegister);
 };
 
 
